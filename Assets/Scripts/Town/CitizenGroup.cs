a2f4e804f3e810362f0b9a8789e5e8091feb87c4
Alfadas <alfadas.3@gmail.com>
2019-04-27 14:08:03 +0200

    Fixed Light in every Scene, added base planes,  Added spawning citizens to collect as new slaves, added simple Slave Counter

---
+++ Assets/Scripts/Town/CitizenGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenGroup : MonoBehaviour
+{
+    int citizenCount;
+    public SpawnPoint spawnPoint;
+    [SerializeField] int minCount = 2;
+    [SerializeField] int maxCount = 25;
+    [SerializeField] int displayTime = 15;
+
+    public SlaveManager SlaveManager { get;  set; }
+
+    private void Start()
+    {
+        citizenCount = Random.Range(minCount, maxCount + 1);
+        StartCoroutine(Disappear());
+    }
+
+    private void OnMouseDown()
+    {
+        SlaveManager.AddSlaves(citizenCount);
+        spawnPoint.CitizenGroup = null;
+        Object.Destroy(gameObject);
+    }
+
+    IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(displayTime);
+        spawnPoint.CitizenGroup = null;
+        Object.Destroy(gameObject);
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Town/SlaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlaveManager : MonoBehaviour
+{
+    [SerializeField] Text SlaveCountDisplay;
+
+    int slaveCount = 0;
+    [SerializeField] int initalSlaveCount;
+
+    private void Start()
+    {
+        slaveCount = initalSlaveCount;
+        SlaveCountDisplay.text = "Slave Count: " + slaveCount;
+    }
+
+    public int SlaveCount
+    {
+        get
+        {
+            return slaveCount;
+        }
+    }
+    public void AddSlaves(int count)
+    {
+        slaveCount += count;
+        SlaveCountDisplay.text = "Slave Count: " + slaveCount;
+    }
+    public void RemoveSlaves(int count)
+    {
+        slaveCount -= count;
+        SlaveCountDisplay.text = "Slave Count: " + slaveCount;
+    }
+}
