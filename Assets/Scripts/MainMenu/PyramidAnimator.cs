858e9402f3b8d452334c9f161c211366ce0daa2f
kruemelkeksfan <a_maximilian@web.de>
2019-04-28 23:07:26 +0200

    Added main menu draft

---
+++ Assets/Scripts/MainMenu/PyramidAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidAnimator : MonoBehaviour
+	{
+	private const float STAGE_TIME = 0.5f;
+
+	private GameObject pyramid = null;
+	private List<GameObject> pyramidstages = null;
+	private int currentstage = 1;
+	private float passedtime = 0.0f;
+
+	void Start()
+		{
+		pyramid = GameObject.Find("Pyramid");
+
+		pyramidstages = new List<GameObject>();
+		foreach(Transform stage in pyramid.transform)
+			{
+			if(stage.gameObject.name != "PyramidFoundation")
+				{
+				stage.gameObject.SetActive(false);
+				}
+			pyramidstages.Add(stage.gameObject);
+			}
+		}
+
+	void Update()
+		{
+		passedtime += Time.deltaTime;
+
+		if(passedtime > STAGE_TIME && currentstage < pyramidstages.Count)
+			{
+			pyramidstages[currentstage].SetActive(true);
+			++currentstage;
+			passedtime = 0.0f;
+			}
+		}
+	}
