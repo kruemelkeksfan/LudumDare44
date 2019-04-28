220f679d3b61ba2d35b50bc62e6f8f08bb7a4e5e
Alfadas <alfadas.3@gmail.com>
2019-04-28 09:35:18 +0200

    accidentally worked in mainScean, added querry

---
+++ Assets/CollectorManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectorManager : WorkingManager
+{
+    [SerializeField] protected Text materialCount;
+    [SerializeField] protected Text materialText;
+}
